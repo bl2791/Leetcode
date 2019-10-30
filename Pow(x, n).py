a780028c337719cd49a592d22b6b7faff4e20411
bl2791 <bl2791@columbia.edu>
2019-10-30 15:57:15 -0400

    No. 50

---
+++ Pow(x, n).py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        arr = []
+        if n == 0:
+            return 1
+        
+        if n < 0:
+            x = 1/x
+            n = -n
+            
+        while True:
+            loop = 1
+            num = x
+            while loop < n:
+                num = num * num
+                loop = 2 * loop
+            if loop == n:
+                arr.append(num)
+                break
+            arr.append(math.sqrt(num))
+            n = n - loop/2
+        result = 1
+        for i in arr:
+            result = result * i
+        return result
